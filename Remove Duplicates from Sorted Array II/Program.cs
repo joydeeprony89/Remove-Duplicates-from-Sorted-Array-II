a144573783737fe76380e8807bffbbeccaafa96b
jd_786 <jaydeep_shil@certree.com>
2022-12-17 13:47:25 +0530

    Add project files.

---
+++ Remove Duplicates from Sorted Array II/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Solution s = new Solution();
+var nums = new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+var answer = s.RemoveDuplicates(nums);
+Console.WriteLine(answer);  
+
+public class Solution
+{
+  public int RemoveDuplicates(int[] nums)
+  {
+    int i = 0;
+    foreach (int n in nums)
+    {
+      if (i < 2 || n > nums[i - 2])
+        nums[i++] = n;
+    }
+    return i;
+  }
+}
